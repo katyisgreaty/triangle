0b8531ff5a6715b9c8f57e71c0cb5cc450596c9e
Katy Daviscourt and Nicole Sanders <katy@>
2017-01-12 15:27:37 -0800

    separate out business and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
+var triangleName = function(sideOneInput, sideTwoInput, sideThreeInput) {
+  if ((sideOneInput + sideTwoInput <= sideThreeInput) || (sideOneInput + sideThreeInput <= sideTwoInput) || (sideTwoInput + sideThreeInput <= sideOneInput)) {
+    return "NOT a triangle."
+  } else {
+    if (sideOneInput === sideTwoInput &&  sideOneInput === sideThreeInput){
+      return "Equilateral."
+    } else if ((sideOneInput === sideTwoInput && sideOneInput !== sideThreeInput) || (sideOneInput === sideThreeInput && sideOneInput !== sideTwoInput) || (sideTwoInput === sideThreeInput && sideOneInput !== sideTwoInput)){
+      return "Isosceles."
+    } else if (sideOneInput !== sideTwoInput && sideTwoInput !== sideThreeInput && sideOneInput !== sideThreeInput) {
+      return "Scalene."
+    }
+  }
+}
+
+
 $(document).ready(function(){
   $("#triangle").submit(function(event){
     event.preventDefault();
-    var sideOneInput = parseInt($("#side1").val());
-    var sideTwoInput = parseInt($("#side2").val());
-    var sideThreeInput = parseInt($("#side3").val());
-
-    if ((sideOneInput + sideTwoInput <= sideThreeInput) || (sideOneInput + sideThreeInput <= sideTwoInput) || (sideTwoInput + sideThreeInput <= sideOneInput)) {
-      $("#result").text("This is NOT a triangle.")
-    } else {
-        if (sideOneInput === sideTwoInput &&  sideOneInput === sideThreeInput){
-          $("#result").text("Equilateral.")
-        } else if ((sideOneInput === sideTwoInput && sideOneInput !== sideThreeInput) || (sideOneInput === sideThreeInput && sideOneInput !== sideTwoInput) || (sideTwoInput === sideThreeInput && sideOneInput !== sideTwoInput)){
-          $("#result").text("Isosceles.")
-        } else if (sideOneInput !== sideTwoInput && sideTwoInput !== sideThreeInput && sideOneInput !== sideThreeInput) {
-          $("#result").text("Scalene.")
-        }
-    }
+    var a = parseInt($("#side1").val());
+    var b = parseInt($("#side2").val());
+    var c = parseInt($("#side3").val());
+    $("#result").text("This triangle is " + triangleName(a,b,c));
   });
 });
